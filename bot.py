906598c1cdeec42b125582fccfd79ad2c9ebb9c7
pbd88 <pobedin88@gmail.com>
2018-12-14 13:52:26 +0400

    модифицирован принцип обработки данных, установленны webhooks и ssl сертификаты

--- bot.py
+++ bot.py
@@ -9,13 +9,12 @@ import qr
 import os
 from dbQuery import Query
 
+import cherrypy
 
 
 bd = Query()
 bot = telebot.TeleBot(config.token)
 bot_name = telebot.TeleBot(config.bot_name)
-
-# Дописать проверку на ДР
 
 
 # Начало диалога
@@ -80,8 +79,8 @@ def gifts(message):
             msg = "Название: " + i['name'] + ". Статус: " + i['status']
             bot.send_message(message.chat.id, msg, reply_markup=markups.keyboardMain)
 
-        #bot.send_message(message.chat.id, messages.s_gifts + utils.get_users_gifts(bd, message.chat.id),
-        #                 reply_markup=markups.keyboardMain)
+        # bot.send_message(message.chat.id, messages.s_gifts + utils.get_users_gifts(bd, message.chat.id),
+        # reply_markup=markups.keyboardMain)
 
 
 @bot.message_handler(commands=["Пригласить_друга"])
@@ -105,5 +104,53 @@ def something_wrong(message):
     bot.send_message(message.chat.id, messages.s_text, reply_markup=markups.keyboardMain)
 
 
+#--------
+
+WEBHOOK_HOST = '95.213.236.53'     # используемый ip сервера
+WEBHOOK_PORT = 8443               # 443, 80, 88 или 8443 (порт должен быть открыт!)
+WEBHOOK_LISTEN = '95.213.236.53'   # На некоторых серверах придется указывать такой же IP, что и выше
+
+WEBHOOK_SSL_CERT = 'ssl/webhook_cert.pem'  # Путь к сертификату
+WEBHOOK_SSL_PRIV = 'ssl/webhook_pkey.pem'  # Путь к приватному ключу
+
+WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
+WEBHOOK_URL_PATH = "/%s/" % (config.token)
+
+
+# Наш вебхук-сервер
+class WebhookServer(object):
+    @cherrypy.expose
+    def index(self):
+        if 'content-length' in cherrypy.request.headers and \
+                        'content-type' in cherrypy.request.headers and \
+                        cherrypy.request.headers['content-type'] == 'application/json':
+            length = int(cherrypy.request.headers['content-length'])
+            json_string = cherrypy.request.body.read(length).decode("utf-8")
+            update = telebot.types.Update.de_json(json_string)
+            # Эта функция обеспечивает проверку входящего сообщения
+            bot.process_new_updates([update])
+            return ''
+        else:
+            raise cherrypy.HTTPError(403)
+# -----------------
+
 if __name__ == '__main__':
-    bot.polling(none_stop=True)
+    # bot.polling(none_stop=True)
+
+    # Снимаем вебхук перед повторной установкой (избавляет от некоторых проблем)
+    bot.remove_webhook()
+
+    # Ставим заново вебхук
+    bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH, certificate=open(WEBHOOK_SSL_CERT, 'r'))
+
+    # Указываем настройки сервера CherryPy
+    cherrypy.config.update({
+        'server.socket_host': WEBHOOK_LISTEN,
+        'server.socket_port': WEBHOOK_PORT,
+        'server.ssl_module': 'builtin',
+        'server.ssl_certificate': WEBHOOK_SSL_CERT,
+        'server.ssl_private_key': WEBHOOK_SSL_PRIV
+    })
+
+     # Собственно, запуск!
+    cherrypy.quickstart(WebhookServer(), WEBHOOK_URL_PATH, {'/': {}})
